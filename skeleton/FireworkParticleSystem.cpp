69d132897029256c05aa1eb5c93413c6c09af43e
bingoo30 <biwang01@ucm.es>
2025-10-30 19:39:15 +0100

    firework

---
+++ skeleton/FireworkParticleSystem.cpp
@@ -0,0 +1,10 @@
+#include "FireworkParticleSystem.h"
+
+FireworkParticleSystem::FireworkParticleSystem(const Particle_Data& pd, const Particle_Deviation_Data& pdd, int n)
+{
+}
+
+bool FireworkParticleSystem::check_out_of_limit(Particle* p) const
+{
+	return false;
+}
---
+++ skeleton/FireworkParticleSystem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ParticleSystem.h"
+class FireworkParticleSystem :
+    public ParticleSystem 
+{
+public:
+    FireworkParticleSystem(const Particle_Data& pd, const Particle_Deviation_Data& pdd, int n);
+protected:
+    bool check_out_of_limit(Particle* p) const override;
+
+};
+
