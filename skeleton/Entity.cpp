8a5690385dfbb02d60f9a2d1d25139dd8f0e3f9c
bingoo30 <biwang01@ucm.es>
2025-11-02 13:53:13 +0100

    arreglando entity

--- skeleton/Entity.cpp
+++ skeleton/Entity.cpp
@@ -9,20 +9,35 @@ Entity::Entity(
     double vol,
     double lt,
     double m)
-    : transform(new physx::PxTransform(p)),
+    : transform(PxTransform(p)),
     color(c), 
-    masa(m),
+    mass(m),
+    shape(s),
     lifetime(lt),
     age(0.0),
     volume(vol),
-    renderItem(new RenderItem(s, transform, color))
+    renderItem(nullptr),
+    renderItemRegisted(false)
 {
+}
+
+Entity::Entity(const physx::PxVec3& p, const physx::PxVec4& c, physx::PxShape* s, double vol, double lt, double m, bool create)
+    : transform(PxTransform(p)),
+    color(c),
+    mass(m),
+    shape(s),
+    lifetime(lt),
+    age(0.0),
+    volume(vol),
+    renderItem(nullptr),
+    renderItemRegisted(false)
+{
+    if (create) create_renderItem();
 }
 
 Entity::~Entity()
 {
-	DeregisterRenderItem(renderItem);
-	delete transform;
+    derregisterRenderItem();
 }
 void Entity::update(double t) {
 	integrate(t);
@@ -32,4 +47,24 @@ void Entity::update(double t) {
 void Entity::update_lifetime(double t)
 {
 	age += t;
+}
+
+void Entity::derregisterRenderItem()
+{
+    if (renderItem && renderItemRegisted) {
+        DeregisterRenderItem(renderItem.get());
+        renderItemRegisted = false;
+    }
+}
+void Entity::create_renderItem()
+{
+    derregisterRenderItem();
+
+    renderItem = std::make_unique<RenderItem>(shape, &transform, color);
+    RegisterRenderItem(renderItem.get());
+    renderItemRegisted = true;
+}
+bool Entity::isValidRenderItem() const
+{
+    return renderItem != nullptr && renderItemRegisted;
 }
--- skeleton/Entity.h
+++ skeleton/Entity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <PxPhysicsAPI.h>
+#include <memory>
 #include "RenderUtils.hpp"
 class Entity
 {
@@ -14,29 +15,46 @@ public:
 		FIREWORK_ROCKET,
 		FIREWORK_SPARK
 	};
-	//p =posicion inicial
-	//c =color
-	//s =forma de la geometria
-	Entity(const physx::PxVec3& p = physx::PxVec3(0), const physx::PxVec4& c = physx::PxVec4(1), physx::PxShape* s=nullptr, double vol=1.0, double lt = -1.0, double m = 0.0);
+
+	Entity(const physx::PxVec3& p = physx::PxVec3(0), 
+		   const physx::PxVec4& c = physx::PxVec4(1), 
+		   physx::PxShape* s=nullptr, 
+		   double vol=1.0, 
+		   double lt = -1.0, 
+		   double m = 0.0);
+	Entity(const physx::PxVec3& p = physx::PxVec3(0),
+		const physx::PxVec4& c = physx::PxVec4(1),
+		physx::PxShape* s = nullptr,
+		double vol = 1.0,
+		double lt = -1.0,
+		double m = 0.0,
+		bool create = false);
 	virtual ~Entity();
 	virtual void update(double t);
-	RenderItem* getRenderItem() { return renderItem; };
+	RenderItem* getRenderItem() { return renderItem.get(); };
 	bool isDead() const { return (lifetime > 0.0 && age >= lifetime); };
 
-	double getMass() const { return masa; };
+	double getMass() const { return mass; };
 	double getLifeTime() const { return lifetime; };
 	double getVol() const { return volume; };
+	physx::PxShape* getShape() const { return shape; };
 protected:
 	virtual void integrate(double t) = 0;
 	void update_lifetime(double t);
-	RenderItem* renderItem;
-	physx::PxTransform* transform;	// posición
+	void derregisterRenderItem();
+	void create_renderItem();
+	bool isValidRenderItem() const;
+	std::unique_ptr<RenderItem> renderItem;
+	physx::PxTransform transform;
 	physx::PxVec4 color;	// color
-	double masa;  //masa
+	double mass;  //masa
+	physx::PxShape* shape;
 
 	//para quitarlo del vector si lleva mucho tiempo en la escena
 	double lifetime;  // segundos de vida (-1 = infinito)
 	double age;       // tiempo acumulado
 	double volume; //volumen de la particula (solo se usa para crear particulas hijas)
+private:
+	bool renderItemRegisted;
 };
 
